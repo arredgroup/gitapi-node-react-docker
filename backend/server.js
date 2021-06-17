b30b6895beda0012890feb7e7e9c9ed7e3ba35f1
Christopher Arredondo <arredgroup@gmail.com>
2021-06-17 11:01:40 -0400

    NodeJS connection with Github API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
+
+const gitapi = require("./services/gitapi")
 
 // Constants
 const PORT = 8080;
@@ -9,7 +12,14 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    gitapi.getInfoRepo().then(data => {
+        res.setHeader('Content-Type', 'text/plain');
+        res.send(data);
+    }).catch(function (e) {
+        res.status(500, {
+            error: e
+        });
+    });
 });
 
 app.listen(PORT, HOST);
---
+++ backend/services/gitapi.js
@@ -0,0 +1,27 @@
+
+const https = require("https");
+const config = {
+    headers: {'user-agent': 'node.js'},
+    host: "api.github.com",
+    port: 443,
+    path: "/repos/arredgroup/gitapi-node-react-docker/commits",
+    method: 'GET'
+}
+module.exports = {
+    getInfoRepo(){
+        return new Promise((resolve, reject) => {
+            const response = https.request(config, (resp) => {
+                let data = '';
+                // A chunk of data has been received.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                   resolve(JSON.parse(data));
+                });
+            });
+            response.end();
+        })
+    }
+}
